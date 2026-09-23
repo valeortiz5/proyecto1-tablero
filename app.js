2bed90e60a9cbd0a2c0fc8d846736c7eb960d301
Gabrielrtega <alcides.ortega2026@adoc.superate.org>
2026-09-23 14:20:23 -0600

    feat(luces): activa modo niebla

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ btn.addEventListener('click', () => {
  vel.textContent = "Velocidad: 20 km/h (Marcha mínima)";
  btn.style.backgroundColor = "#10b981";
 });
+// Estudiante B activa luces
+const luces = document.getElementById('luces');
+luces.style.backgroundColor = "#eab308";
+luces.style.color = "#000000";
+luces.textContent = "Luces: Encendidas (Modo Niebla)";
